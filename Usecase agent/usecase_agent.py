11f238a17cf06d176545c3138613fd31d39224d6
Sreecharan Mamidi <102374440+Sreecharan2027@users.noreply.github.com>
2025-04-14 20:45:26 +0530

    Update usecase_agent.py

--- Usecase agent/usecase_agent.py
+++ Usecase agent/usecase_agent.py
@@ -6,8 +6,8 @@ import os
 import faiss
 import numpy as np
 from fpdf import FPDF
-#from serpapi import GoogleSearch
-from serpapi import GoogleSearchResults
+from serpapi import GoogleSearch
+#from serpapi import GoogleSearchResults
 
 load_dotenv()
 co = cohere.Client(os.getenv("COHERE_API_KEY"))
